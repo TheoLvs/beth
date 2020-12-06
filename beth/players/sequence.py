02418c637538314d4c3ae58abf2d144cee71f5e9
Theo Alves Da Costa <theo.alves.da.costa@gmail.com>
2020-12-06 19:27:01 +0100

    Created SequencePlayer, an abstraction class to read from sequence of moves and play them

---
+++ beth/players/sequence.py
@@ -0,0 +1,35 @@
+
+
+from .player import Player
+
+class SequenceGame:
+    def __init__(self,sequence,wait,victory_status = None,winner = None):
+
+        self.victory_status = victory_status
+        self.winner = winner
+
+        if isinstance(sequence,str): sequence = sequence.split(" ")
+        self.sequence = sequence
+        seq_white = sequence[::2]
+        seq_black = sequence[1::2]
+
+        # Init two players
+        self.white = SequencePlayer(seq_white,wait = wait)
+        self.black = SequencePlayer(seq_black,wait = wait)
+
+
+class SequencePlayer(Player):
+    def __init__(self,sequence,**kwargs):
+
+        super().__init__(**kwargs)
+        self.sequence = sequence
+        self.turn_number = 0
+
+    def move(self,value):
+
+        self.wait()
+
+        move = self.sequence[self.turn_number]
+        self.turn_number += 1
+        self.board.push_san(move) 
+
